b0424b5365457546f2b65e0b5c371e60ce1dc275
Michael Smith <michaelsmith1994@yahoo.com>
2024-12-13 14:48:58 +0000

    another lab completed

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,25 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int[] arr3;
+        if(arr1 != null && arr2 != null){
+            int array1len = arr1.length;
+            int array2len = arr2.length;
+            
+            //Avoiding using the array library for this ill be iterating manually with loops as this is a practical.
 
-        return null;
+            arr3 = new int[array1len+array2len];//set a length to it of arr1 + arr2
+            
+            for(int i = 0; i<array1len; i++){
+                //setting my arr1 values
+                arr3[i] = arr1[i];          
+            }
+            for(int i = 0; i < array2len; i++){
+                //setting my arr2 values
+                arr3[array1len + i] = arr2[i];
+            }
+            return arr3;
+        }
+        return null; // return a null if im sent an empty array from main
     }
 }
